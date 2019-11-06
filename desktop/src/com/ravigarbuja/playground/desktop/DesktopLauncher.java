cbc482bf565ee6059e92bf7dd0927e47ca854ce2
Ravi Garbuja Pun <-v>
2019-11-06 21:37:30 +0545

    Initial Commit

---
+++ desktop/src/com/ravigarbuja/playground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ravigarbuja.playground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ravigarbuja.playground.PlaygroundGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlaygroundGame(), config);
+	}
+}
